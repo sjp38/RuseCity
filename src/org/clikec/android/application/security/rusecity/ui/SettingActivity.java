c623701b0baf1a7a1737f220f8f7fb1c3c93462f
SeongJae Park <sj38.park@gmail.com>
2011-08-24 00:37:32 +0900

    [2011.08.24][sjp38] First commit of RuseCity.

---
+++ src/org/clikec/android/application/security/rusecity/ui/SettingActivity.java
@@ -0,0 +1,13 @@
+package org.clikec.android.application.security.rusecity.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
